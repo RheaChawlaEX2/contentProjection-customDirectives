9e761740c47df1166f08ae51ccb6a7aabb842d52
RheaChawlaEX2 <rchawla@ex2india.com>
2022-04-26 11:00:44 +0530

    Final Changes

--- src/app/inputString.directive.ts
+++ src/app/inputString.directive.ts
@@ -8,24 +8,21 @@ export class StringDirective {
   
    
   regex = '^[a-zA-Z]+$';
-   initialValue : any;
+  
   constructor(private inputElement: ElementRef) {
-    this.initialValue = inputElement.nativeElement.value;
-    
   }
   @HostListener('keypress',['$event']) onKeyPress(event : any) {
    
    return new RegExp(this.regex).test(event.key) || event.keyCode == 32 ;
   }
 
+
   @HostListener('keyup.space',['$event'])
   onKeyUp(event:KeyboardEvent){
-    if(this.onKeyPress(event) ===true){
+    if(this.onKeyPress(event) === true){
       this.inputElement.nativeElement.value = this.titleCaseFormat(this.inputElement.nativeElement.value); 
     } 
-    else{
-      this.inputElement.nativeElement.value = this. titleCaseFormat(this.initialValue);
-    }
+    
   }
 
   titleCaseFormat(inputString: any){
